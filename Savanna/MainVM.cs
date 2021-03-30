4c7c488786cfa84ff7f2a5368ef967f1e667a499
Liaihua <ilyavovchenko431@gmail.com>
2021-03-30 10:00:39 +1000

    Добавьте файлы проекта.

---
+++ Savanna/MainVM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Savanna
+{
+    [Serializable]
+    public class MainVM : MainWindow
+    {
+        ContactsDB contactsDB;
+        public MainVM()
+        {
+            //ComboBox.Items.Add("убирай");
+            ContactsDB contactsDB = new ContactsDB();
+            // 2) заполнение выпадющего списка (combobox) не сделано
+        }
+        public void ShowContacts(List<Contact> contacts)
+        {
+            contacts.Clear();//1
+            List<Contact> list = new List<Contact>();//криво 2 
+            for (int i = 0; i < list.Count; i++)
+            {
+                Contact contact = (Contact)list[i];
+                contacts.Add(contact);
+
+            }
+
+        }
+    }
+}
---
+++ Savanna/MobilePhone.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Savanna
+{
+    [Serializable]
+    public class MobilePhone : IPhone
+    {
+        //public MobilePhone(string number)
+        //{
+        //   Number = number;
+        //}
+
+        public string Number { get; set; }
+
+        public string GetTypeName(string Number)
+        {
+            return "мобильный телефон";
+        }
+    }
+}
